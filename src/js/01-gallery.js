d14b2408b4a7c5e4546b0845e2e2d0f4a1334b9d
bolt2000 <bolt2000@ukr.net>
2023-03-04 21:14:59 +0200

    +1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,39 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+const div = document.querySelector('.gallery');
+const cardsMarkup = creatImageCards(galleryItems);
+
+
+function creatImageCards(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+       <li>
+       <img
+       class="gallery__image"
+       href="${original}"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+       >
+       </li>
+        `;
+    }).join('');
+}
+
+div.insertAdjacentHTML('beforeend', cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
