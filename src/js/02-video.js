fc4dc74f7e6bb4a978a01f614307d9d76715e7f9
bolt2000 <bolt2000@ukr.net>
2023-03-05 19:50:28 +0200

    +2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+};
+
+// player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedData = localStorage.getItem("videoplayer-current-time");
+const parsedData = JSON.parse(savedData);
+console.log('Data', parsedData);
+
+player.setCurrentTime(parsedData.seconds).then(function(seconds) {
+  console.log('Секунди', seconds); 
+});
+
